7ade60c2d130a7c5ba8d92b345b4674af989c3d1
Devon Estes <devon.c.estes@gmail.com>
2016-11-02 16:57:21 +0100

    first files

--- lib/materialized_view.rb
+++ lib/materialized_view.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This is the module that has our single public API method
 module MaterializedView
   class << self
@@ -9,13 +10,30 @@ module MaterializedView
       end
     end
 
+    private
+
     def execute_with_dependencies(view, schema)
       execute "REFRESH MATERIALIZED VIEW #{schema}.#{view}"
     end
 
     def execute(sql)
-      puts 'EXECUTING'
       puts sql
     end
+
+    DEPENDENCY_SQL = <<-SQL.freeze
+			SELECT r_ns.nspname || '.' || cl_r.relname AS materialized_view,
+			array_agg(d_ns.nspname || '.' || cl_d.relname) AS depends_on
+			FROM pg_rewrite AS r
+			JOIN pg_class AS cl_r ON r.ev_class=cl_r.oid
+			JOIN pg_depend AS d ON r.oid=d.objid
+			JOIN pg_class AS cl_d ON d.refobjid=cl_d.oid
+			JOIN pg_namespace AS r_ns ON r_ns.oid = cl_r.relnamespace
+			JOIN pg_namespace AS d_ns ON d_ns.oid = cl_d.relnamespace
+			WHERE cl_d.relkind = 'm'
+			AND cl_r.relkind = 'm'
+			AND cl_d.relname != cl_r.relname
+			GROUP BY cl_r.relname, r_ns.nspname
+			ORDER BY cl_r.relname;
+    SQL
   end
 end
--- materialized_views.gemspec
+++ materialized_views.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'materialized_views'
   s.version     = '0.1.0'
-  s.date        = '2016-11-2'
+  s.date        = '2016-11-02'
   s.summary     = 'Convenience methods to help with updating materialized views'
   s.description = 'Convenience methods to help with updating materialized views'
   s.authors     = ['Devon Estes']
   s.email       = 'devon.c.estes@gmail.com'
-  s.files       = ['lib/materialized_views.rb']
+  s.files       = ['lib/materialized_view.rb']
   s.homepage    = 'https://www.github.com/devonestes/materialized_views'
   s.license     = 'MIT'
 end
